ab561dd955de576fbb5dc0ddefd44db6303368fd
Venkat Sandeep <sandeep.kv92@gmail.com>
2015-12-08 16:59:04 -0500

    extracting IDs of movies with atleast 100 votes

--- imdb/data/id_extractor.py
+++ imdb/data/id_extractor.py
@@ -4,8 +4,8 @@ This script extracts the IDs of around 14000 movies from sorted in the order of 
 import urllib2
 from BeautifulSoup import BeautifulSoup as BS
 
-for i in range(1,14000,50):
-    url = "http://www.imdb.com/search/title?at=0&languages=en%7C1&num_votes=1000,&sort=user_rating&start="+str(i)+"&title_type=feature"
+for i in range(1,33300,50):
+    url = "http://www.imdb.com/search/title?at=0&languages=en%7C1&num_votes=100,&sort=user_rating&start="+str(i)+"&title_type=feature"
     html = urllib2.urlopen(url).read()
     soup = BS(html)
     elem = soup.findAll('span',{'class':'wlb_wrapper'})
