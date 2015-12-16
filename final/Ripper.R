a85f5bc68cdb0c2267161eb8208209397a16b497
Saravanan SJ <sjs7007@gmail.com>
2015-12-16 12:42:21 -0500

    adding final scripts

---
+++ final/Ripper.R
@@ -0,0 +1,103 @@
+
+#install.packages("caret")
+library(caret)
+#install.packages("e1071")
+library(e1071)
+#install.packages("klaR")
+library(klaR)
+#install.packages("RWeka")
+library(RWeka)
+#install.packages("class")
+library(class)
+#install.packages("cluster")
+library(cluster)
+#install.packages("ROCR")
+library(ROCR)
+#install.packages("ggplot2")
+library(ggplot2)
+#install.packages("rpart")
+library(rpart)
+
+
+set.seed(05713)
+
+  
+  movie=read.csv("data.csv")
+  temp_data = movie
+  
+  #View(movies)
+  #View(temp_data)
+  
+  partition = sample(1:nrow(temp_data), size=0.8*nrow(temp_data))
+  
+  # Split data into train and test
+  train_data = temp_data[partition,]
+  
+  test_data = temp_data[-partition,]
+ 
+  
+ 
+
+
+
+
+
+
+  
+  modelJRip <- JRip(genres.1~., train_data[,c("cast.1","director.1","genres.1","genres.2","genres.3","genres.4","producer.1","writer.1")])
+  
+
+
+
+
+  predictions <- predict(modelJRip, test_data[,c("cast.1","director.1","genres.1","genres.2","genres.3","genres.4","producer.1","writer.1")])
+  
+  confusionMatrix(predictions,test_data$genres.1)
+
+
+
+  x<-confusionMatrix(kNNpred,test_data$genres.1)
+  
+  confusion<-as.table(x)
+  
+  overall.accuracy <- x$overall['Accuracy']
+  
+  
+  precisionVector = numeric() #empty vector 
+  for (i in 1:ncol(confusion)) {
+    precisionVector <- c(precisionVector,confusion[i,i]/sum(confusion[i,]))
+  }
+  
+  
+  
+  #recall = true positive / true positive + false negative 
+  recallVector = numeric() #empty vector 
+  for (i in 1:ncol(confusion)) {
+    recallVector <- c(recallVector,confusion[i,i]/sum(confusion[,i]))
+  }
+  
+  #f Measure = 2rp / r+p
+  fMeasureVector = (2 * recallVector * precisionVector)/(recallVector+precisionVector)
+  
+  
+  precisionVector
+  recallVector
+  fMeasureVector
+  
+  
+
+
+
+##Detaching packages
+
+
+detach("package:ggplot2", unload=TRUE)
+detach("package:caret", unload=TRUE)
+detach("package:class", unload=TRUE)
+detach("package:RWeka", unload=TRUE)
+detach("package:datasets", unload=TRUE)
+detach("package:cluster", unload=TRUE)
+detach("package:ROCR", unload=TRUE)
+detach("package:e1071", unload=TRUE)
+detach("package:rpart", unload=TRUE)
+detach("package:randomForest", unload=TRUE)
